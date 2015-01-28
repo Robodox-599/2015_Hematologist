ad5f08c43254684424b1aa30980d31d0a79a44ab
CFrank13 <fconnor13@gmail.com>
2015-01-28 15:19:05 -0800

    Encoder Initialized

--- src/HematologistManipulator.cpp
+++ src/HematologistManipulator.cpp
@@ -6,8 +6,8 @@ HematologistManipulator::HematologistManipulator()
 	leftLiftMotor = new Talon(0);
 	rightLiftMotor = new Talon(1);
 	secondTierSol = new DoubleSolenoid(2, 3);
-	leftLiftEncoder = new Encoder(4, 5, false);
-	rightLiftEncoder = new Encoder(6, 7, false);
+	leftLiftEncoder = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
+	rightLiftEncoder = new Encoder(2, 3, false, Encoder::EncodingType::k4X);
 	manipulatorJoystick = new Joystick(2);
 }
 
@@ -72,6 +72,11 @@ void HematologistManipulator::secondTierSolBackward()
 void HematologistManipulator::secondTierSolStop()
 {
 	secondTierSol->Set(DoubleSolenoid::kOff);
+}
+
+void HematologistManipulator::endcoder()
+{
+	rightLiftEncoder->Start();
 }
 
 
