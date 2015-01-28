a4eb4c0dbed38833f8306fa73411661d2caf4119
jasmin3del <jasmin3delgado@gmail.com>
2015-01-28 15:30:07 -0800

    finished i believe

--- src/Macros.h
+++ src/Macros.h
@@ -2,6 +2,6 @@
 #define MACROS_H_
 
 #define MANIP_JOYSTICK						(0)			//TODO: get correct number
-#define TOGGLE_BIN_HUGGER_BUTTON		(1)			//TODO: DO THAT ^
+#define TOGGLE_BIN_HUGGER_BUTTON			(1)			//TODO: DO THAT ^
 
 #endif
