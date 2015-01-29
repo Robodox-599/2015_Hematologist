368ac15216020d2e70dd16c3edc3c91b95ad38a0
jasmin3del <jasmin3delgado@gmail.com>
2015-01-28 17:13:41 -0800

    more operator interface changes

---
+++ src/OperatorInterface.cpp
@@ -0,0 +1,20 @@
+#include "OperatorInterface.h"
+
+OperatorInterface::OperatorInterface()
+{
+	dashboard->init();
+}
+
+virtual OperatorInterface::~OperatorInterface()
+{
+
+}
+
+OperatorInterface::printForkliftPower()
+{
+	//if (leftForkliftMotor->Set(power))
+	//{
+	//	dashboard->PutNumber("Forklift Power", power );
+	//}
+
+}
