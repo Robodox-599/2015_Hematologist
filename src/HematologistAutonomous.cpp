4f0b3c6d0f5f3bf05ef08c4233197ba8bb66eae9
CFrank13 <fconnor13@gmail.com>
2015-01-31 12:03:19 -0800

    encoder reverse direction was not necessary so removed

--- src/HematologistAutonomous.cpp
+++ src/HematologistAutonomous.cpp
@@ -14,25 +14,21 @@ HematologistAutonomous::HematologistAutonomous()
 	frontLeftEnc->Encoder::SetMaxPeriod(1);
 	frontLeftEnc->Encoder::SetMinRate(10);
 	frontLeftEnc->Encoder::SetDistancePerPulse(5);
-	frontLeftEnc->Encoder::SetReverseDirection(true);
 	frontLeftEnc->Encoder::SetSamplesToAverage(7);
 
 	frontRightEnc->Encoder::SetMaxPeriod(1);
 	frontRightEnc->Encoder::SetMinRate(10);
 	frontRightEnc->Encoder::SetDistancePerPulse(5);
-	frontRightEnc->Encoder::SetReverseDirection(true);
 	frontRightEnc->Encoder::SetSamplesToAverage(7);
 
 	backLeftEnc->Encoder::SetMaxPeriod(1);
 	backLeftEnc->Encoder::SetMinRate(10);
 	backLeftEnc->Encoder::SetDistancePerPulse(5);
-	backLeftEnc->Encoder::SetReverseDirection(true);
 	backLeftEnc->Encoder::SetSamplesToAverage(7);
 
 	backRightEnc->Encoder::SetMaxPeriod(1);
 	backRightEnc->Encoder::SetMinRate(10);
 	backRightEnc->Encoder::SetDistancePerPulse(5);
-	backRightEnc->Encoder::SetReverseDirection(true);
 	backRightEnc->Encoder::SetSamplesToAverage(7);
 }
 
