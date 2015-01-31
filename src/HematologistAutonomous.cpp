53b3f020f77bf45311c2f0587038570c107f20b9
CFrank13 <fconnor13@gmail.com>
2015-01-31 11:40:51 -0800

    made encoder for autonomous dirving

--- src/HematologistAutonomous.cpp
+++ src/HematologistAutonomous.cpp
@@ -4,6 +4,37 @@ HematologistAutonomous::HematologistAutonomous()
 {
 	man = new HematologistManipulator();
 	drive = new HematologistDrive();
+
+	averageEncVal = 0;
+
+	frontLeftEnc = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
+	frontRightEnc = new Encoder(3, 4, true, Encoder::EncodingType::k4X);
+	backLeftEnc = new Encoder(5, 6, false, Encoder::EncodingType::k4X);
+	backRightEnc = new Encoder(7, 8, true, Encoder::EncodingType::k4X);
+
+	frontLeftEnc->Encoder::SetMaxPeriod(1);
+	frontLeftEnc->Encoder::SetMinRate(10);
+	frontLeftEnc->Encoder::SetDistancePerPulse(5);
+	frontLeftEnc->Encoder::SetReverseDirection(true);
+	frontLeftEnc->Encoder::SetSamplesToAverage(7);
+
+	frontRightEnc->Encoder::SetMaxPeriod(1);
+	frontRightEnc->Encoder::SetMinRate(10);
+	frontRightEnc->Encoder::SetDistancePerPulse(5);
+	frontRightEnc->Encoder::SetReverseDirection(true);
+	frontRightEnc->Encoder::SetSamplesToAverage(7);
+
+	backLeftEnc->Encoder::SetMaxPeriod(1);
+	backLeftEnc->Encoder::SetMinRate(10);
+	backLeftEnc->Encoder::SetDistancePerPulse(5);
+	backLeftEnc->Encoder::SetReverseDirection(true);
+	backLeftEnc->Encoder::SetSamplesToAverage(7);
+
+	backRightEnc->Encoder::SetMaxPeriod(1);
+	backRightEnc->Encoder::SetMinRate(10);
+	backRightEnc->Encoder::SetDistancePerPulse(5);
+	backRightEnc->Encoder::SetReverseDirection(true);
+	backRightEnc->Encoder::SetSamplesToAverage(7);
 }
 
 HematologistAutonomous::~HematologistAutonomous()
@@ -13,10 +44,13 @@ HematologistAutonomous::~HematologistAutonomous()
 
 void HematologistAutonomous::secondHemanAuto()
 {
-	man->liftEncoder->Encoder::Reset();
+	frontLeftEnc->Encoder::Reset();
+	frontRightEnc->Encoder::Reset();
+	backLeftEnc->Encoder::Reset();
+	backRightEnc->Encoder::Reset();
 	man->secondTierClawOpen();
 
-	if(man->liftEncoder->Get() < 20)
+	if(averageEncVal < 20)
 	{
 		drive->frontLeftMotor->Set(.5);
 		drive->frontRightMotor->Set(-.5);
@@ -26,7 +60,7 @@ void HematologistAutonomous::secondHemanAuto()
 	}
 	else
 	{
-		if(man->liftEncoder->Get() < 25)
+		if(averageEncVal < 25)
 		{
 			drive->frontLeftMotor->Set(.5);
 			drive->frontRightMotor->Set(.5);
@@ -34,4 +68,9 @@ void HematologistAutonomous::secondHemanAuto()
 			drive->backRightMotor->Set(.5);
 		}
 	}
+}
+
+void HematologistAutonomous::setAverageEncVal()
+{
+	(frontLeftEnc-> Get() + frontRightEnc-> Get() + backLeftEnc-> Get() + backRightEnc-> Get()) / 4 = averageEncVal;
 }
--- src/HematologistAutonomous.h
+++ src/HematologistAutonomous.h
@@ -6,10 +6,17 @@ class HematologistAutonomous
 private:
 	HematologistManipulator *man;
 	HematologistDrive* drive;
+	Encoder* frontLeftEnc; //encoders for drive motor
+	Encoder* frontRightEnc;
+	Encoder* backLeftEnc;
+	Encoder* backRightEnc;
+
+	double averageEncVal; //all four Enc Value divided by four
 
 public:
 	HematologistAutonomous();
 	~HematologistAutonomous();
 
 	void secondHemanAuto(); // gets 2-3 totes and stack it
+	void setAverageEncVal();
 };
