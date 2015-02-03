22f54d62692ea7b49a01bbed6e48e0b85c0d001a
Amy Contreras <amycontreras9@yahoo.com>
2015-02-02 16:11:48 -0800

    creation of main file

---
+++ src/Hematologist.cpp
@@ -0,0 +1,19 @@
+#include "WPILib.h"
+
+class Hematologist: public IterativeRobot
+{
+private:
+	void RobotInit(){}
+
+	void AutonomousInit(){}
+
+	void AutonomousPeriodic(){}
+
+	void TeleopInit(){}
+
+	void TeleopPeriodic(){}
+
+	void TestPeriodic(){}
+};
+
+START_ROBOT_CLASS(Hematologist);
