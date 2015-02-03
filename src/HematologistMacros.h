e83241dedcb65340fbf1f5e3175af7c10f8b88e0
Amy Contreras <amycontreras9@yahoo.com>
2015-02-02 17:14:33 -0800

    macros numbers, also includes wpilib so that all files will include macros to include wpilib

---
+++ src/HematologistMacros.h
@@ -0,0 +1,40 @@
+#ifndef HEMATOLOGIST_MACROS_H_
+#define HEMATOLOGIST_MACROS_H_
+#include "WPILib.h"
+
+//Joystick
+#define MANIP_JOYSTICK						(0)		//TODO: FIND CORRECT VALUE
+#define BIG_HUGGER_OPEN_BUTTON				(1)		//TODO: FIND CORRECT VALUE		
+#define BIG_HUGGER_CLOSE_BUTTON				(2)		//TODO: FIND CORRECT VALUE
+#define FORKLIFT_UP_BUTTON					(3)		//TODO: FIND CORRECT VALUE
+#define FORKLIFT_DOWN_BUTTON				(4)		//TODO: FIND CORRECT VALUE
+
+//Motor Channels
+#define FRONT_LEFT_MOTOR_CHANNEL			(2)
+#define BACK_LEFT_MOTOR_CHANNEL				(3)
+#define FRONT_RIGHT_MOTOR_CHANNEL			(0)
+#define BACK_RIGHT_MOTOR_CHANNEL			(1)
+#define LEFT_LIFT_MOTOR_CHANNEL				(5)		//TODO: get correct number
+#define RIGHT_LIFT_MOTOR_CHANNEL			(6)		//TODO: get correct number
+#define LEFT_FORKLIFT_MOTOR_CHANNEL			(7)		//TODO: get correct number
+#define RIGHT_FORKLIFT_MOTOR_CHANNEL		(8)		//TODO: get correct number
+
+//Solenoids
+#define BIN_HUGGER_SOL_CHANNEL_A			(0)		//TODO: get correct number
+#define BIN_HUGGER_SOL_CHANNEL_B			(1)		//TODO: get correct number
+#define SECOND_TIER_SOL_CHANNEL_A			(2)		//TODO: get correct number
+#define SECOND_TIER_SOL_CHANNEL_B			(3)		//TODO: get correct number
+
+//Encoders
+#define LIFT_ENCODER_CHANNEL_A				(0)		//TODO: get correct number
+#define LIFT_ENCODER_CHANNEL_B				(1)		//TODO: get correct number
+#define FORKLIFT_ENCODER_CHANNEL_A		(2)		//TODO: get correct number
+#define FORKLIFT_ENCODER_CHANNEL_B		(3)		//TODO: get correct number
+
+//Misc Constants
+#define DEADZONE                   		    (.1)
+#define LIFT_DEADZONE          				(10)
+#define PRE_SET_LOW							(0)		//TODO: get correct number
+#define PRE_SET_MID							(30)	//TODO: get correct number
+#define PRE_SET_HIGH						(50)	//TODO: get correct number
+
