0db4fbb9b7cbc292e28d69f286c5cf3d6e156b2e
Patrick Artounian <partounian@gmail.com>
2015-02-02 19:04:20 -0800

    Changed new Josystick to Joystick

--- src/HematologistOperatorIterface.cpp
+++ src/HematologistOperatorIterface.cpp
@@ -2,8 +2,8 @@
 
 HematologistOperatorInterface::HematologistOperatorInterface()
 {
-	leftDriveJoystick = new Josytick(LEFT_DRIVE_JOYSTICK);
-	rightDriveJoystick = new Josytick(RIGHT_DRIVE_JOYSTICK);
+	leftDriveJoystick = new Joystick(LEFT_DRIVE_JOYSTICK);
+	rightDriveJoystick = new Joystick(RIGHT_DRIVE_JOYSTICK);
 	manipJoystick = new Joystick(MANIP_JOYSTICK);
 	dashboard->init();
 }
