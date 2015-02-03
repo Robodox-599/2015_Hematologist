40f300777ba694bf121ef1579fcee35acb43e520
cfrank13 <fconnor13@gmail.com>
2015-02-02 18:03:18 -0800

    Modified macro ports

--- src/HematologistMacros.h
+++ src/HematologistMacros.h
@@ -10,14 +10,12 @@
 #define FORKLIFT_DOWN_BUTTON				(4)			//TODO: get correct number
 
 //Motor Channels
-#define FRONT_LEFT_MOTOR_CHANNEL			(2)
-#define BACK_LEFT_MOTOR_CHANNEL				(3)
-#define FRONT_RIGHT_MOTOR_CHANNEL			(0)
-#define BACK_RIGHT_MOTOR_CHANNEL			(1)
-#define LEFT_LIFT_MOTOR_CHANNEL				(5)			//TODO: get correct number
-#define RIGHT_LIFT_MOTOR_CHANNEL			(6)			//TODO: get correct number
-#define LEFT_FORKLIFT_MOTOR_CHANNEL			(7)			//TODO: get correct number
-#define RIGHT_FORKLIFT_MOTOR_CHANNEL		(8)			//TODO: get correct number
+#define BACK_LEFT_MOTOR_CHANNEL				(0)
+#define FRONT_LEFT_MOTOR_CHANNEL			(1)
+#define BACK_RIGHT_MOTOR_CHANNEL			(2)
+#define FRONT_RIGHT_MOTOR_CHANNEL			(3)
+#define LEFT_LIFT_MOTOR_CHANNEL				(4)			//TODO: get correct number
+#define RIGHT_LIFT_MOTOR_CHANNEL			(5)			//TODO: get correct number
 
 //Solenoids
 #define BIN_HUGGER_SOL_CHANNEL_A			(0)			//TODO: get correct number
