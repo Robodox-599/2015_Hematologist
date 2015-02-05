9376725da18665d9bb003398326717557423b9cb
CFrank13 <fconnor13@gmail.com>
2015-02-04 16:34:09 -0800

    added autonomous file, changed the name of function for forklift claw

---
+++ src/HematologistAutonomous.h
@@ -0,0 +1,21 @@
+#ifndef HEMATOLOGIST_AUTONOMOUS_H_
+#define HEMATOLOGIST_AUTONOMOUS_H_
+
+#include "WPILib.h"
+
+class HematologistAutonomous
+{
+private:
+	HematologistManipulator *man;
+	HematologistDrive* drive;
+	Encoder* frontLeftEnc; //encoders for drive motor
+	Encoder* frontRightEnc;
+	Encoder* backLeftEnc;
+	Encoder* backRightEnc;
+
+public:
+	HematologistAutonomous();
+	~HematologistAutonomous();
+
+	void secondHemanAuto(); // gets 2 totes and stack it
+};
