2917bc5856c52a91ef37bda1d6e89f3a718a1e6e
Patrick Artounian <partounian@gmail.com>
2015-02-09 15:21:54 -0800

    Updated main file for testing.

--- src/Hematologist.cpp
+++ src/Hematologist.cpp
@@ -1,9 +1,22 @@
- #include "HematologistMacros.h"
+#include "HematologistMacros.h"
+#include "HematologistOperatorInterface.h"
+#include "HematologistDrive.h"
+#include "HematologistManipulator.h"
 
 class Hematologist: public IterativeRobot
 {
 private:
-	void RobotInit(){}
+	HematologistOperatorInterface* oi;
+	HematologistManipulator* manip;
+	HematologistDrive* drive;
+
+
+	void RobotInit(){
+		oi = new HematologistOperatorInterface();
+		manip = new HematologistManipulator(oi->getJoystick('M'));
+		drive = new HematologistDrive(oi);
+
+	}
 
 	void AutonomousInit(){}
 
@@ -11,7 +24,13 @@ private:
 
 	void TeleopInit(){}
 
-	void TeleopPeriodic(){}
+	void TeleopPeriodic(){
+		//drive->drive(oi->getJoystick('L')->GetY(), oi->getJoystick('L')->GetX(), oi->getJoystick('R')->GetX());
+		manip->moveLift(oi->getJoystick('L')->GetY());
+		oi->getDashboard()->PutNumber("Joystick Y:", oi->getJoystick('L')->GetY());
+		oi->getDashboard()->PutNumber("Left Lift:", manip->leftLiftMotor->GetRaw());
+		oi->getDashboard()->PutNumber("Right Lift:", manip->rightLiftMotor->GetRaw());
+	}
 
 	void TestPeriodic(){}
 };
