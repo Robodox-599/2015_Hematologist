6ce03c64cea9d5de058072c113294efea5503da8
Hyunkyu Lee <hkl902@gmail.com>
2015-02-11 23:52:05 -0800

    testing purposes; had to reverse the strafe joystick as the strafing was inverted

--- src/Hematologist.cpp
+++ src/Hematologist.cpp
@@ -2,6 +2,7 @@
 #include "HematologistOperatorInterface.h"
 #include "HematologistDrive.h"
 #include "HematologistManipulator.h"
+#include "HematologistAutonomous.h"
 
 class Hematologist: public IterativeRobot
 {
@@ -9,27 +10,44 @@ private:
 	HematologistOperatorInterface* oi;
 	HematologistManipulator* manip;
 	HematologistDrive* drive;
+	HematologistAutonomous* auton;
 
 
 	void RobotInit(){
 		oi = new HematologistOperatorInterface();
 		manip = new HematologistManipulator(oi->getJoystick('M'));
 		drive = new HematologistDrive(oi);
+		auton = new HematologistAutonomous(drive, manip);
 
 	}
 
 	void AutonomousInit(){}
 
-	void AutonomousPeriodic(){}
+	void AutonomousPeriodic(){
+		auton->secondHemanAuto();
+		oi->getDashboard()->PutNumber("FrontRightEncoder", drive->frontRightEncoder->Get());
+		oi->getDashboard()->PutNumber("frontLeftEncoder", drive->frontLeftEncoder->Get());
+		oi->getDashboard()->PutNumber("backRightEncoder", drive->backRightEncoder->Get());
+		oi->getDashboard()->PutNumber("backLeftEncoder", drive->backLeftEncoder->Get());
+
+
+	}
 
 	void TeleopInit(){}
 
 	void TeleopPeriodic(){
-		drive->drive(oi->getJoystick('L')->GetY(), oi->getJoystick('L')->GetX(), oi->getJoystick('R')->GetX());
+		drive->drive(oi->getJoystick('L')->GetY(), oi->getJoystick('L')->GetX(), -oi->getJoystick('R')->GetX());
 		manip->moveLift(oi->getJoystick('R')->GetY());
-		oi->getDashboard()->PutNumber("Joystick Y:", oi->getJoystick('L')->GetY());
-		oi->getDashboard()->PutNumber("Left Lift:", manip->leftLiftMotor->GetRaw());
-		oi->getDashboard()->PutNumber("Right Lift:", manip->rightLiftMotor->GetRaw());
+		oi->getDashboard()->PutNumber("Joystick Left Y:", oi->getJoystick('L')->GetY());
+		oi->getDashboard()->PutNumber("Joystick Left X:", oi->getJoystick('L')->GetX());
+		oi->getDashboard()->PutNumber("Joystick Right Y:", oi->getJoystick('R')->GetY());
+		oi->getDashboard()->PutNumber("Joystick Right X:", oi->getJoystick('R')->GetX());
+		oi->getDashboard()->PutNumber("Joystick Manip Y:", oi->getJoystick('M')->GetX());
+		oi->getDashboard()->PutNumber("Joystick Manip Y:", oi->getJoystick('M')->GetY());
+		oi->getDashboard()->PutNumber("FrontRightEncoder", drive->frontRightEncoder->Get());
+		oi->getDashboard()->PutNumber("frontLeftEncoder", drive->frontLeftEncoder->Get());
+		oi->getDashboard()->PutNumber("backRightEncoder", drive->backRightEncoder->Get());
+		oi->getDashboard()->PutNumber("backLeftEncoder", drive->backLeftEncoder->Get());
 	}
 
 	void TestPeriodic(){}
