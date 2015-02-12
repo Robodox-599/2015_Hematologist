f0fb2687d1782066dbd032df98658a4337261108
Hyunkyu Lee <hkl902@gmail.com>
2015-02-11 23:53:48 -0800

    necessary includes, updated constructor to be able to use drive and manip

--- src/HematologistAutonomous.h
+++ src/HematologistAutonomous.h
@@ -1,7 +1,9 @@
 #ifndef HEMATOLOGIST_AUTONOMOUS_H_
 #define HEMATOLOGIST_AUTONOMOUS_H_
 
-#include "WPILib.h"
+#include "HematologistMacros.h"
+#include "HematologistManipulator.h"
+#include "HematologistDrive.h"
 
 class HematologistAutonomous
 {
@@ -10,10 +12,12 @@ private:
 	HematologistDrive* drive;
 
 public:
-	HematologistAutonomous();
+	HematologistAutonomous(HematologistDrive* drive, HematologistManipulator* manip);
 	~HematologistAutonomous();
 	
 	void firstHemanAuto();
 	void secondHemanAuto();
 	void thirdHemanAuto(); // gets 3 totes and stack it
 };
+
+#endif
