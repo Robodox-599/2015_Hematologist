b219d5a3b429af6affb8a079a3309c4d082674a7
Hyunkyu Lee <hkl902@gmail.com>
2015-02-14 10:16:38 -0800

    used a bool in if statement, not a comparison of bools when chcking limitswitch

--- src/HematologistManipulator.cpp
+++ src/HematologistManipulator.cpp
@@ -39,7 +39,7 @@ HematologistManipulator::~HematologistManipulator()
 
 void HematologistManipulator::moveLift(float speed)
 {
-	if (limitSwitch->limitSwitchIsPressed() == false)
+	if (!limitSwitch->limitSwitchIsPressed())
 	{
 
 		if (speed > DEADZONE || speed < -DEADZONE)
