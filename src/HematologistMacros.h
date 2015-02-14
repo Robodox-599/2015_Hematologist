4064eac60ca49dfbffb17ab6da8d6e733372cabc
Hyunkyu Lee <hkl902@gmail.com>
2015-02-14 11:35:44 -0800

    updated macros values following Chris's notebook

--- src/HematologistMacros.h
+++ src/HematologistMacros.h
@@ -51,12 +51,12 @@ Manip Joystick:
 
 
 //Motor Channels
-#define FRONT_LEFT_MOTOR_CHANNEL			(2)
-#define BACK_LEFT_MOTOR_CHANNEL				(3)
-#define FRONT_RIGHT_MOTOR_CHANNEL			(0)
-#define BACK_RIGHT_MOTOR_CHANNEL			(1)
-#define LEFT_LIFT_MOTOR_CHANNEL				(5)		//TODO: get correct number
-#define RIGHT_LIFT_MOTOR_CHANNEL			(6)		//TODO: get correct number
+#define FRONT_LEFT_MOTOR_CHANNEL			(0)
+#define BACK_LEFT_MOTOR_CHANNEL				(1)
+#define FRONT_RIGHT_MOTOR_CHANNEL			(3)
+#define BACK_RIGHT_MOTOR_CHANNEL			(2)
+#define LEFT_LIFT_MOTOR_CHANNEL				(5)		
+#define RIGHT_LIFT_MOTOR_CHANNEL			(6)		
 #define LEFT_FORKLIFT_MOTOR_CHANNEL			(7)		//TODO: get correct number
 #define RIGHT_FORKLIFT_MOTOR_CHANNEL		(8)		//TODO: get correct number
 
@@ -92,8 +92,7 @@ Manip Joystick:
 #define PRE_SET_MID							(30)	//TODO: get correct number
 #define PRE_SET_HIGH						(50)	//TODO: get correct number
 #define LIFT_DEADZONE						(10.0)	//TODO: get correct number
-#define LIMIT_SWITCH_CHANNEL				(10)	//TODO: get numberrrrr
-
+#define LIMIT_SWITCH_CHANNEL				(1)	
 
 //Gyro
 #define GYRO_KP								(.1)
