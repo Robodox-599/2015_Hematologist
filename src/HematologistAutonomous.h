92f277f4058891d3da2c5ed1a41643d7de63026c
Patrick Artounian <partounian@gmail.com>
2015-02-14 09:54:31 -0800

    Added Auto files from autonomous branch.

---
+++ src/HematologistAutonomous.h
@@ -0,0 +1,25 @@
+#ifndef HEMATOLOGIST_AUTONOMOUS_H_
+#define HEMATOLOGIST_AUTONOMOUS_H_
+
+#include "HematologistMacros.h"
+#include "HematologistManipulator.h"
+#include "HematologistDrive.h"
+
+class HematologistAutonomous
+{
+private:
+	HematologistManipulator *manip;
+	HematologistDrive* drive;
+
+public:
+	HematologistAutonomous(HematologistDrive* drive, HematologistManipulator* manip);
+	~HematologistAutonomous();
+	
+	void firstHemanAuto();
+	void secondHemanAuto();
+	void thirdHemanAuto(); // gets 3 totes and stack it
+
+	int encoderAverage();
+};
+
+#endif
