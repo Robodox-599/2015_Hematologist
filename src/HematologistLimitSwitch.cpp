54d7121d9fb829667b8b8988aefd095791cb615d
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-26 22:18:30 -0700

    constructor/destructor of analog limit switch

--- src/HematologistLimitSwitch.cpp
+++ src/HematologistLimitSwitch.cpp
@@ -1,2 +1,12 @@
 #include "HematologistLimitSwitch.h"
 
+HematologistLimitSwitch::HematologistLimitSwitch(int limitSwitchChannel)
+{
+	ai = new AnalogInput(limitSwitchChannel);
+}
+
+HematologistLimitSwitch::~HematologistLimitSwitch()
+{
+	delete ai;
+	ai = NULL;
+}
