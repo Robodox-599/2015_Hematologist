9e0b31ef6d44b241ea80b668f3c8ee9737f28251
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-26 21:37:53 -0700

    initialize forward, turn, strafe

--- src/HematologistDrive.cpp
+++ src/HematologistDrive.cpp
@@ -11,4 +11,8 @@ HematologistDrive::HematologistDrive()
 	frontRightEncoder = new Encoder(FRONT_RIGHT_ENCODER_CHANNEL_A, FRONT_RIGHT_ENCODER_CHANNEL_B);
 	backLeftEncoder = new Encoder(BACK_LEFT_ENCODER_CHANNEL_A, BACK_LEFT_ENCODER_CHANNEL_B);
 	backRightEncoder = new Encoder(BACK_RIGHT_ENCODER_CHANNEL_A, BACK_RIGHT_ENCODER_CHANNEL_B);
+
+	forward = 0;
+	turn = 0;
+	strafe = 0;
 }
