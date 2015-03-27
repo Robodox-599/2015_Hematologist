557aa49e077c04e116c7046efeb9d96004245484
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-26 21:15:22 -0700

    get/set functions for forward/turn/strafe declared

--- src/HematologistDrive.h
+++ src/HematologistDrive.h
@@ -9,7 +9,13 @@ public:
 	HematologistDrive();
 	~HematologistDrive();
 
-	
+	void setForward(float forward);
+	void setTurn(float turn);
+	void setStrafe(float strafe);
+
+	float getForward();
+	float getTurn();
+	float getStrafe();
 
 private:
 	Talon* frontLeftMotor;
