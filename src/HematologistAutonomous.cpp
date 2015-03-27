2bd5056e1a5b21912340399a77fc9f85da74174b
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-27 01:01:10 -0700

    constructor/destructor made, created all the objects

--- src/HematologistAutonomous.cpp
+++ src/HematologistAutonomous.cpp
@@ -1,1 +1,19 @@
 #include "HematologistAutonomous.h"
+
+HematologistAutonomous::HematologistAutonomous(HematologistOperatorInterface* oi, HematologistManipulator* manip, HematologistDrive* drive)
+{
+	this->oi = oi;
+	this->manip = manip;
+	this->drive = drive;
+}
+
+HematologistAutonomous::~HematologistAutonomous()
+{
+	delete oi;
+	delete manip;
+	delete drive;
+	oi = NULL;
+	manip = NULL;
+	drive = NULL;
+}
+
