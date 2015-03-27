57e18dd1dde666f07647f033d3f94f75379f3282
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-27 00:54:05 -0700

    added the other files besides just macros to .h file of auto

--- src/HematologistAutonomous.h
+++ src/HematologistAutonomous.h
@@ -2,6 +2,9 @@
 #define HEMATOLOGIST_AUTONOMOUS_H
 
 #include "HematologistMacros.h"
+#include "HematologistOperatorInterface.h"
+#include "HematologistDrive.h"
+#include "HematologistManipulator.h"
 
 class HematologistAutonomous
 {
