e2a8ccb08128827e573983b4710e27abb7499255
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-27 01:49:46 -0700

    macros update, just don't have what ubtton does what

--- src/HematologistMacros.h
+++ src/HematologistMacros.h
@@ -1,1 +1,85 @@
 #include "WPILib.h"
+
+/*
+	Manipulator Joystick
+		1.
+		2.
+		3.
+		4.
+		5.
+		6.
+		7.
+		8.
+		9.
+		10.
+		11.
+		12.
+	Drive Joystick
+		1.
+		2.
+		3.
+		4.
+		5.
+		6.
+		7.
+		8.
+		9.
+		10.
+		11.
+		12.
+
+*/
+
+//Joysticks
+#define MANIP_JOYSTICK_PORT							(0)
+#define LEFT_DRIVE_JOYSTICK_PORT					(1)
+#define RIGHT_DRIVE_JOYSTICK_PORT					(2)
+
+
+
+//Motors
+#define FRONT_LEFT_MOTOR_CHANNEL					(0)
+#define BACK_LEFT_MOTOR_CHANNEL						(1)
+#define BACK_RIGHT_MOTOR_CHANNEL					(2)
+#define FRONT_RIGHT_MOTOR_CHANNEL					(3)
+#define LEFT_LIFT_MOTOR_CHANNEL						(5)
+#define RIGHT_LIFT_MOTOR_CHANNEL					(6)
+#define LEFT_ROLLER_MOTOR_CHANNEL					(7)
+#define RIGHT_ROLLER_MOTOR_CHANNEL					(8)
+
+//Encoders
+#define BACK_RIGHT_ENCODER_CHANNEL_A				(0)
+#define BACK_RIGHT_ENCODER_CHANNEL_B				(1)
+#define FRONT_RIGHT_ENCODER_CHANNEL_A				(2)
+#define FRONT_RIGHT_ENCODER_CHANNEL_B				(3)
+#define FRONT_LEFT_ENCODER_CHANNEL_A				(4)
+#define FRONT_LEFT_ENCODER_CHANNEL_B				(5)
+#define BACK_LEFT_ENCODER_CHANNEL_A					(6)
+#define BACK_LEFT_ENCODER_CHANNEL_B					(7)
+#define LIFT_ENCODER_CHANNEL_A						(8)				
+#define LIFT_ENCODER_CHANNEL_B						(9)
+
+//Pneumatics
+#define FORKLIFT_PISTON_CHANNEL_A					(0)				
+#define FORKLIFT_PISTON_CHANNEL_B					(1)
+#define LONG_ARM_PISTON_CHANNEL_B					(2)
+#define LONG_ARM_PISTON_CHANNEL_A					(3)
+#define BIN_HUGGER_PISTON_CHANNEL_B					(4)
+#define BIN_HUGGER_PISTON_CHANNEL_A					(5)
+#define SECOND_TIER_PISTON_CHANNEL_B				(6)
+#define SECOND_TIER_PISTON_CHANNEL_A				(7)
+
+#define FLAP_OPEN_RELAY_CHANNEL						(0)
+#define FLAP_CLOSE_RELAY_CHANNEL					(1)	
+
+//misc
+#define COMPRESSOR_CHANNEL							(0)	
+#define TOP_LIMIT_SWITCH_CHANNEL					(0)
+#define BOTTOM_LIMIT_SWITCH_CHANNEL					(2)
+#define DEADZONE									(.05)
+#define LIFT_DEADZONE								(.1)
+#define ENCODER_DEADZONE							(20)
+
+
+
+
