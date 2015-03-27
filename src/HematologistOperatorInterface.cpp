fd6cc8fea7f9c9dc8b240cc302af5181de84ebc2
Hyunkyu Lee <hkl0902@gmail.com>
2015-03-26 22:57:54 -0700

    created/initialized all of the joysticks/dashboard

--- src/HematologistOperatorInterface.cpp
+++ src/HematologistOperatorInterface.cpp
@@ -1,2 +1,9 @@
 #include "HematologistOperatorInterface.h"
 
+HematologistOperatorInterface::HematologistOperatorInterface()
+{
+	leftDriveJoystick 	= new Joystick(LEFT_DRIVE_JOYSTICK_PORT);
+	rightDriveJoystick	= new Joystick(RIGHT_DRIVE_JOYSTICK_PORT);
+	manipJoystick		= new Joystick(MANIP_JOYSTICK_PORT);
+	dashboard->init(); 
+}
