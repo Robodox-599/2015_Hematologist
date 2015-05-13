b9ded7e593bdbf5e4c5b5c5e04a6ab1d6e21894b
Patrick Artounian <partounian@gmail.com>
2015-05-13 15:41:17 -0700

    explains functions/variables in autonomous.h

--- src/HematologistAutonomous.h
+++ src/HematologistAutonomous.h
@@ -9,17 +9,25 @@
 class HematologistAutonomous
 {
 public:
+	//Send in these Hematologist Objects rather than creating new ones
+	//If you create them again, you'll create a memory conflict
+	//You need these to control the robot
 	HematologistAutonomous(HematologistOperatorInterface* oi, HematologistManipulator* manip, HematologistDrive* drive);
 	~HematologistAutonomous();
 
+	//Send in true to strafe right, false to strafe left	
 	void strafe(bool right);
+
+	//Moves back, flips long arm back, flips flaps open, moves forward with two bins
 	void longArmAuto();
 
 private:
+	//I need these Hematologist Objects to control the robot
 	HematologistOperatorInterface* oi;
 	HematologistManipulator* manip;
 	HematologistDrive* drive;
 
+	//These two variables are used to keep track of where in the sequence the robot is in
 	int driveStep;	
 	int manipStep;
 
