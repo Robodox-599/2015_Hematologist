4fbf25ce1ffc37d4abf1e7a6033931dcb222e91d
Hyunkyu Lee <hkl0902@gmail.com>
2015-06-08 22:11:43 -0700

    explains why the limit swtich is programmed with a class I made and not a class from the WPILibrary and the logic behind in making it

--- src/HematologistLimitSwitch.h
+++ src/HematologistLimitSwitch.h
@@ -3,15 +3,24 @@
 
 #include "HematologistMacros.h"
 
+/*
+    this class is special
+    for one limitswitches are not usually coded this way and the only reason why we did it here is because we ran out of digital ports
+    so, we had to use an analog port which mean't sautering for electornics and a really weird coding problem for software
+    here's how it works
+*/
 class HematologistLimitSwitch
 {
 public:
 	HematologistLimitSwitch(int limitSwitchChannel);
 	~HematologistLimitSwitch();
 
+    //tells us if the limit switch is pressed. the logic is more talked about in the .cpp file
 	bool isPressed();
 private:
-	AnalogInput* ai;
+    //because we had to use an analog port, that meant we had to read the analog port
+    //this analog input lets us do that
+	AnalogInput* ai;  
 };
 
 #endif
